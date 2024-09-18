6c57c341d77bf82a687c1c722e4b0e7a977a9628
liz <65708133+wlqqlz@users.noreply.github.com>
2024-09-18 09:48:55 +0100

    initial

---
+++ src/main/java/net/javaguides/spring/ioc/Application.java
@@ -0,0 +1,12 @@
+package net.javaguides.spring.ioc;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        GreetingService greetingService = (GreetingService) context.getBean("greetingService");
+        greetingService.getMessage();
+    }
+}
